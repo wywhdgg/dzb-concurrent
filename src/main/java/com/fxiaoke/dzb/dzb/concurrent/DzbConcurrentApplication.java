d4ed2735c6077353d6a49e5832391d1d936c75e3
dongzhb <dongzhb@fxiaoke.com>
2019-05-16 19:46:01 +0800

    代码提交

---
+++ src/main/java/com/fxiaoke/dzb/dzb/concurrent/DzbConcurrentApplication.java
@@ -0,0 +1,11 @@
+package com.fxiaoke.dzb.dzb.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzbConcurrentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DzbConcurrentApplication.class, args);
+    }
+}
